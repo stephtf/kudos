ffe50999ef89e547fa0b27ee53d4ce7af9a3c5e2
Stephanie Fajardo <stfajardo@gmail.com>
2022-07-22 20:50:43 -0400

    images upload successfully

--- client/src/app/components/image-upload/image-upload.component.ts
+++ client/src/app/components/image-upload/image-upload.component.ts
@@ -7,11 +7,11 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./image-upload.component.scss']
 })
 export class ImageUploadComponent implements OnInit {
-// newImageFileData: any;
 newImageFile: any;
 imageName: any; 
 awsData: any; 
 imageUrl: any;
+testImage = `https://kudos2u.s3.amazonaws.com/04d1b1e0fedf87868f07a4051507719a`;
 
 @Output() imageEvent = new EventEmitter<string>();
 
@@ -21,68 +21,31 @@ private getUrl = 'http://localhost:3001/s3Url'
   constructor(private httpClient: HttpClient) { }
 
   ngOnInit(): void {
-
   }
 
 handleImageInput = (event: Event) => {
     this.newImageFile = (<HTMLInputElement>event.target).files; 
     this.imageName = this.newImageFile[0].name; 
-   
-    // console.log(this.newImageFile);
 }  
 
-
-
 sendImage() {
-  this.imageEvent.emit(this.imageName);
-  
-  // KudoComponent.kudoData.img_url = this.imageName;
-  // KudoComponent.newKudoPost(); 
-
   // grab the AWS secure link
   this.httpClient.get(this.getUrl).subscribe(response => {
-    this.awsData = response;
+  this.awsData = response;
+  this.httpClient.put(this.awsData.url, this.newImageFile[0]).subscribe(data => data);
 
-    this.httpClient.put(this.awsData.url, this.newImageFile[0]).subscribe(data => data);
-    
+  // update db with imageUrl
+  this.imageUrl = this.awsData.url.slice(0,65);
+  this.imageEvent.emit(this.imageUrl);
 
-    // .split('https://kudos2u.s3.amazonaws.com/')[1].slice(0,32)
-
-    // this.imageUrl = this.awsData.url.split(0,65);
-
-  
   });
 
-  // this.postImage()
+}
+
 }
 
 
 
-// postImage() {
-//   this.httpClient.put(this.awsData.url, this.newImageFile[0]).subscribe(data => data);
-//   // this.bucketImage = this.awsData.url.split('?')[0];
-// }
 
 
 
-}
-
-// client-side: 
-// don't forget to import this function generateUploadUrl() into the client side
-// here we are fetching the url from the server 
-// const { url } = await fetch("/s3Url").then(res => res.json()) 
-// console.log(url) 
- 
-// put request to post an update to the S3 bucket 
-// await fetch(url, {
-//     method: "PUT",
-//     headers: {
-//         "Content-Type": "multipart/form-data"
-//     },
-//     body: this.newImageFile
-// })
-
-// upload new url to mongodb databse with user ID, then awaiting that, post the imageUrl to the html 
-// make some value true, for when this first step is completed.  
-// const imageUrl = url.split('?')[0]
-// console.log(imageUrl); 
